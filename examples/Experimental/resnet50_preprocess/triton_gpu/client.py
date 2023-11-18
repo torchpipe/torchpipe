07b93ff2bf4f1112e22a58eb94c3214ecee5e809
tp-nan <hydzhang@hotmail.com>
2023-11-18 02:15:52 +0000

    triton

--- examples/Experimental/resnet50_preprocess/triton_gpu/client.py
+++ examples/Experimental/resnet50_preprocess/triton_gpu/client.py
@@ -7,7 +7,7 @@ import numpy as np
 import tritongrpcclient
 
 class TritonInfer:
-    def __init__(self, args):
+    def __init__(self):
         
         import torchpipe as tp
         
@@ -18,7 +18,7 @@ class TritonInfer:
         self.model_name = "ensemble_dali_resnet"
         self.url = os.environ.get("TRITON_SERVER_URL", "localhost:8001")
         self.triton_client = tritongrpcclient.InferenceServerClient(url=self.url)
-        self.outputs = tritongrpcclient.InferRequestedOutput(self.output_name)
+        self.outputs = [tritongrpcclient.InferRequestedOutput(self.output_name)]
 
         # # add
 
@@ -62,19 +62,39 @@ class TritonInfer:
 
     def run(self, img):
         img_path, img_bytes = img[0]
+        img_np = np.frombuffer(img_bytes, dtype=np.uint8)[None, :]
+        #print(len(img_np))
 
         
-        inputs = [tritongrpcclient.InferInput(self.input_name, [len(img_bytes)], "UINT8")]
+        inputs = [tritongrpcclient.InferInput(self.input_name, img_np.shape, "UINT8")]
         # outputs.append(tritongrpcclient.InferRequestedOutput(self.output_name))
 
-        inputs[0].set_data_from_numpy(np.array(img_bytes, dtype=np.uint8))
+        inputs[0].set_data_from_numpy(img_np)
         
         triton_results = self.triton_client.infer(
             model_name=self.model_name,
-            inputs=[inputs],
-            outputs=[self.outputs])
+            inputs=inputs,
+            outputs=self.outputs)
+
+        arr = triton_results.as_numpy( self.output_name )
+        #print(arr.shape)
+        max_value = np.max(arr)
+        max_index = np.argmax(arr)
+        #print(max_index, max_value)
+        return max_value, max_index
 
 
-        output = triton_results.as_numpy( self.output_name )
-        print(output)
-        return output
+
+img_path = "../../../../test/assets/image/gray.jpg"
+img=open(img_path,'rb').read()
+
+num_clients = 40
+forwards = [TritonInfer() for i in range(num_clients)]
+forwards[0].run([(img_path, img)])
+
+from torchpipe.utils.test import test_from_raw_file
+result = test_from_raw_file([x.run for x in forwards], os.path.join("../../../..", "test/assets/encode_jpeg/"),num_clients=40, batch_size=1,total_number=20000)
+
+
+print("\n", result)
+
