8604d87a45afab0e41d9ce75e3c527b11be7eafa
tp-nan <hydzhang@hotmail.com>
2023-12-02 02:38:58 +0000

    a

--- examples/Experimental/no3_graph/export_onnx.py
+++ examples/Experimental/no3_graph/export_onnx.py
@@ -17,3 +17,20 @@ if __name__ == '__main__':
         model_simp, check = onnxsim.simplify("fastervit_0_224_224.onnx")
         assert check, "Simplified ONNX model could not be validated"
         onnx.save(model_simp, "fastervit_0_224_224.onnx")
+
+    # trtexec --onnx=fastervit_0_224_224.onnx --fp16  --minShapes=input:1x3x224x224 --optShapes=input:8x3x224x224 --maxShapes=input:8x3x224x224 --shapes=input:8x3x224x224  --saveEngine=fastervit_0_224_224.trt
+    # 122.463 2.1 ms 
+    # trtexec --loadEngine=fastervit_0_224_224.trt --shapes=input:1x3x224x224 --fp16 
+    # 125.014 1.6ms
+    # trtexec --loadEngine=fastervit_0_224_224.trt --shapes=input:4x3x224x224 --fp16 
+    # 133.82 1.8
+
+    # trtexec --onnx=fastervit_0_224_224.onnx --fp16  --minShapes=input:1x3x224x224 --optShapes=input:4x3x224x224 --maxShapes=input:4x3x224x224 --shapes=input:4x3x224x224  --saveEngine=fastervit_0_224_224_4.trt
+    # 125.308 1.525
+    # trtexec --loadEngine=fastervit_0_224_224_4.trt --shapes=input:1x3x224x224 --fp16 
+    # 83.873 1.42727
+
+
+   # trtexec --onnx=fastervit_0_224_224.onnx --fp16  --minShapes=input:1x3x224x224 --optShapes=input:1x3x224x224 --maxShapes=input:1x3x224x224 --shapes=input:1x3x224x224  --saveEngine=fastervit_0_224_224_1.trt
+    # 195.043 0.963574 
+    # trtexec --loadEngine=fastervit_0_224_224_1.trt --shapes=input:1x3x224x224 --verbose 
---
+++ examples/Experimental/no3_graph/throughput.py
@@ -0,0 +1,35 @@
+
+
+
+import os,torch
+
+
+
+
+if __name__ == "__main__":
+    from torchpipe import pipe, parse_toml, TASK_DATA_KEY, TASK_RESULT_KEY
+     # config["resnet50"]["model::cache"] = f"resnet50_{}_{}.trt"
+    config = {
+        "backend":"SyncTensor[TensorrtTensor]",
+        "instance_num": 1,
+        "model":"fastervit_0_224_224_1.trt"
+    }
+    nodes = pipe(config)
+    img = torch.zeros((1,3,224,224),device="cuda")
+
+    def run(img):
+        for img_path, img_bytes in img:
+            input = {TASK_DATA_KEY: img, "node_name": "jpg_decoder"}
+            nodes(input)
+
+            if TASK_RESULT_KEY not in input.keys():
+                print("error : no result")
+                return
+            z=input[TASK_RESULT_KEY].cpu()
+
+    from torchpipe.utils.test import test_from_raw_file
+
+ 
+    num_clients = 20
+
+    result = test_from_raw_file(run, os.path.join("../../..", "test/assets/encode_jpeg/"),num_clients=num_clients, batch_size=1,total_number=10000)
