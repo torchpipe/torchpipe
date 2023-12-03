1fd1ddeb39d83632a6a11ba9f5e155644766df4c
tp-nan <hydzhang@hotmail.com>
2023-12-03 17:39:28 +0000

    experiment v0

--- torchpipe/csrc/pipeline/include/Jump.hpp
+++ torchpipe/csrc/pipeline/include/Jump.hpp
@@ -41,7 +41,7 @@ class Jump : public Backend {
 
   // void forward(dict);
 
-  uint32_t max() const override final { return UINT32_MAX; };
+  uint32_t max() const override final { return 1; };
 
  protected:
   std::string jump_;
