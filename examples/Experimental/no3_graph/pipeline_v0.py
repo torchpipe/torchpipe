de7d02c0dee74661389b1535dcc3a2a4c4f55a7c
tp-nan <hydzhang@hotmail.com>
2023-12-03 15:44:20 +0000

    experiment v0

--- examples/Experimental/no3_graph/pipeline_v0.py
+++ examples/Experimental/no3_graph/pipeline_v0.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
 
         # detect
         input = {}
-        input["data"] = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0).cuda()
+        input["data"] = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0)
         input["ratio"] = 1.0/ratio
 
 
@@ -84,7 +84,7 @@ if __name__ == "__main__":
             # img -= np.array([0.485, 0.456, 0.406])
             # img /= np.array([0.229, 0.224, 0.225])
             img = img.transpose(2, 0, 1)
-            croped_img.append(torch.from_numpy(img).unsqueeze(0).cuda())
+            croped_img.append(torch.from_numpy(img).unsqueeze(0))
 
 
         # classify
