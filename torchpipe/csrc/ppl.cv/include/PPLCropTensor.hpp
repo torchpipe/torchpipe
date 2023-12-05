4dc831d8b0807092ba40f08ca4e1f3fe8c7ae262
tp-nan <hydzhang@hotmail.com>
2023-12-05 09:05:48 +0000

    pplcv

---
+++ torchpipe/csrc/ppl.cv/include/PPLCropTensor.hpp
@@ -0,0 +1,14 @@
+#include "Backend.hpp"
+#include "params.hpp"
+namespace ipipe {
+class PPLCropTensor : public SingleBackend {
+ public:
+  bool init(const std::unordered_map<std::string, std::string>& config, dict dict_config) override;
+  void forward(dict input_dict) override;
+
+ private:
+  std::unique_ptr<Params> params_;
+  int resize_h_;
+  int resize_w_;
+};
+}  // namespace ipipe
