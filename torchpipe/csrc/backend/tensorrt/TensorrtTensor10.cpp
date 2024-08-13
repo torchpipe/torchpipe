125ff45ba0e2b7432ec6afc2dae91039f8cd3631
nan <hydzhang@hotmail.com>
2024-08-13 09:46:52 +0000

    a

--- torchpipe/csrc/backend/tensorrt/TensorrtTensor10.cpp
+++ torchpipe/csrc/backend/tensorrt/TensorrtTensor10.cpp
@@ -271,7 +271,7 @@ void TensorrtTensor::parse_context(dict dict_config, int _independent_thread_ind
 // std::iota(input_reorder_.rbegin(), input_reorder_.rend(), 0);
 // std::iota(input_reorder_.begin(), input_reorder_.end(), 0);
 #if NV_TENSORRT_MAJOR >= 10
-    std::iota(output_reorder_.begin(), output_reorder_.end(), 0);
+    std::iota(input_reorder_.begin(), input_reorder_.end(), 0);
 #else
     reorder_by_alpha(input_reorder_, engine_);
 #endif
