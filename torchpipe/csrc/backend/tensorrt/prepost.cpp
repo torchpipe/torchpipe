8cb33de485b9bc6a9547035c2bf2f4688880ad7c
nan <hydzhang@hotmail.com>
2024-10-30 12:07:11 +0000

    debug

--- torchpipe/csrc/backend/tensorrt/prepost.cpp
+++ torchpipe/csrc/backend/tensorrt/prepost.cpp
@@ -7,13 +7,7 @@ void TorchPostProcessor::forward(std::vector<torch::Tensor> net_outputs, std::ve
                                  const std::vector<torch::Tensor>& net_inputs) {
   SPDLOG_DEBUG("PostProcessor input size:{}, request size:{}, output[0] size[0]:{}", inputs.size(),
                get_request_size(inputs), net_outputs[0].sizes()[0]);
-  if (inputs.size() == 1) {
-    if (net_outputs.size() == 1)
-      (*inputs[0])[TASK_RESULT_KEY] = net_outputs[0];
-    else
-      (*inputs[0])[TASK_RESULT_KEY] = net_outputs;
-    return;
-  } else if (net_outputs[0].sizes()[0] > inputs.size()) {
+  if (net_outputs[0].sizes()[0] > inputs.size()) {
     std::vector<uint32_t> shapes{0};
     for (const auto& item : inputs) {
       shapes.push_back(get_request_size(item));
@@ -38,6 +32,12 @@ void TorchPostProcessor::forward(std::vector<torch::Tensor> net_outputs, std::ve
       } else
         (*inputs[i])[TASK_RESULT_KEY] = single_result;  // 返回std::vector<torch::Tensor>
     }
+  } else if (inputs.size() == 1) {
+    if (net_outputs.size() == 1)
+      (*inputs[0])[TASK_RESULT_KEY] = net_outputs[0];
+    else
+      (*inputs[0])[TASK_RESULT_KEY] = net_outputs;
+    return;
   } else {
     for (std::size_t i = 0; i < inputs.size(); ++i) {
       std::vector<torch::Tensor> single_result;
