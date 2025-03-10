bc4e518cb91a62a91b4305dcdd3b5070186f72d3
tp-nan <hydzhang@hotmail.com>
2025-03-11 01:01:01 +0800

    ci

--- setup.py
+++ setup.py
@@ -270,6 +270,6 @@ setuptools.setup(
     ext_modules=ext_modules,
     cmdclass=cmdclass,
     # use_scm_version=True,
-    version="1.0.10",
+    version="1.0.11",
     setup_requires=required_setup_deps
 )
