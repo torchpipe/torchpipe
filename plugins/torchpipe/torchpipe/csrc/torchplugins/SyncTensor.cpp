042b25c0406c5982658c9de19026d11f3a0c9ba5
nan <hydzhagn@hotmail.com>
2025-05-07 03:49:49 +0000

    StreamPool

--- plugins/torchpipe/torchpipe/csrc/torchplugins/SyncTensor.cpp
+++ plugins/torchpipe/torchpipe/csrc/torchplugins/SyncTensor.cpp
@@ -19,18 +19,9 @@ void SyncTensor::impl_init(
   HAMI_ASSERT(
       iter != config.end(),
       "You are not in an independent thread mode(TASK_INDEX_KEY was not detected. Maybe use `With[StreamPool, *]` instead");
-  if (iter != config.end()) {
-    independent_thread_index_ = std::stoi(iter->second);
-    HAMI_ASSERT(independent_thread_index_ >= 0);
-  } else {
-    SPDLOG_WARN(
-        "You are not in an independent thread mode(TASK_INDEX_KEY was not "
-        "detected). ");
-    HAMI_ASSERT(
-        dep,
-        "You are not in an independent thread mode, pls set the dependency by " +
-            cls_name + "[*]");
-  }
+
+  independent_thread_index_ = std::stoi(iter->second);
+  HAMI_ASSERT(independent_thread_index_ >= 0);
 
   if (config.find("device_id") != config.end()) {
     throw std::runtime_error(
@@ -41,28 +32,12 @@ void SyncTensor::impl_init(
 
   c10::cuda::getCurrentCUDAStream().synchronize();
 
-  if (independent_thread_index_ >= 0) {
-    bNeedSync_ = torch_not_use_default_stream(device_id_int, true);
-    // Schedule保证了init和forward在同一个线程
-    HAMI_ASSERT(
-        bNeedSync_,
-        "This backend can only be used in default current stream. may be use `With[StreamPool,*]` instead.");
-    // SPDLOG_INFO("SyncTensor: sync enabled={}", bNeedSync_);
-  } else if (
-      c10::cuda::getCurrentCUDAStream(device_id_int) ==
-      c10::cuda::getDefaultCUDAStream(device_id_int)) {
-    // bNeedSync_ = true;
-    stream_ = c10::cuda::getStreamFromPool(true, device_id_int);
+  bNeedSync_ = torch_not_use_default_stream(device_id_int, true);
+  // Schedule保证了init和forward在同一个线程
+  HAMI_ASSERT(
+      bNeedSync_,
+      "This backend can only be used in default current stream. may be use `With[StreamPool,*]` instead.");
 
-    [[maybe_unused]] c10::cuda::CUDAStreamGuard s(*stream_);
-    HAMI_ASSERT(
-        c10::cuda::getCurrentCUDAStream(-1) !=
-        c10::cuda::getDefaultCUDAStream(-1));
-    HAMI_ASSERT(dep);
-
-    owned_backend_ = init_backend(*dep, config, kwargs);
-    c10::cuda::getCurrentCUDAStream().synchronize();
-  }
   if (dep && !owned_backend_) {
     owned_backend_ = init_backend(*dep, config, kwargs);
   }
@@ -74,32 +49,17 @@ void SyncTensor::impl_init(
   return;
 }
 
-// void SyncTensor::post_init(
-//     const std::unordered_map<std::string, std::string>& config,
-//     const dict& kwargs) {
-//   c10::cuda::getCurrentCUDAStream().synchronize();
-// }
-
 void SyncTensor::impl_forward(const std::vector<dict>& ios) {
-  if (independent_thread_index_ >= 0) {
-    impl_dep_forward(ios);
-    if (bNeedSync_)
-      c10::cuda::getCurrentCUDAStream().synchronize();
-  } else if (stream_) {
-    [[maybe_unused]] c10::cuda::CUDAStreamGuard s(*stream_);
-    assert(
-        c10::cuda::getCurrentCUDAStream(-1) !=
-        c10::cuda::getDefaultCUDAStream(-1));
-    assert(stream_ == c10::cuda::getCurrentCUDAStream(-1));
+  if (owned_backend_) {
+    static auto curr_stream = c10::cuda::getCurrentCUDAStream(-1);
+    static auto default_stream = c10::cuda::getDefaultCUDAStream(-1);
 
-    event_.record(s.original_stream());
-    event_.block(s.current_stream());
-
-    owned_backend_->forward(ios);
-    s.current_stream().synchronize();
-  } else {
-    impl_dep_forward(ios);
+    event_.record(default_stream);
+    event_.block(curr_stream);
   }
+  impl_dep_forward(ios);
+
+  c10::cuda::getCurrentCUDAStream().synchronize();
 }
 
 HAMI_REGISTER(hami::Backend, SyncTensor, "SyncTensor,StreamGuard");
