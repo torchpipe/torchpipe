6e8a7fd15bfe67691d276c03c68de7fd5cdec222
nan <hydzhagn@hotmail.com>
2025-08-12 08:57:08 +0000

    d

--- plugins/torchpipe/exp/exp3stability/run.sh
+++ plugins/torchpipe/exp/exp3stability/run.sh
@@ -3,13 +3,6 @@
 
 rm -f stability.log
 
-# resnet
-
-python decouple_eval/benchmark.py  --model resnet101 --preprocess cpu --max 5 --trt_instance_num 2 --timeout 5 --preprocess-instances 6 --total_number 1000000 --client 14 --save stability.log
-
-
-python decouple_eval/benchmark.py  --model resnet101 --preprocess gpu --max 5 --trt_instance_num 2 --timeout 5 --preprocess-instances 6 --total_number 1000000 --client 14 --save stability.log
-
 # mobilenetv2_100
 
 python decouple_eval/benchmark.py  --model mobilenetv2_100 --preprocess cpu --max 6 --trt_instance_num 2 --timeout 5 --preprocess-instances 6 --total_number 1000000 --client 16 --save stability.log
@@ -17,12 +10,22 @@ python decouple_eval/benchmark.py  --model mobilenetv2_100 --preprocess cpu --ma
 
 python decouple_eval/benchmark.py  --model mobilenetv2_100 --preprocess gpu --max 6 --trt_instance_num 2 --timeout 5 --preprocess-instances 10 --total_number 1000000 --client 16 --save stability.log
 
+
 # vit_base_patch16_siglip_224
 
 python decouple_eval/benchmark.py  --model vit_base_patch16_siglip_224 --preprocess cpu --max 3 --trt_instance_num 2 --timeout 5 --preprocess-instances 6 --total_number 1000000 --client 10 --save stability.log
 
 
 python decouple_eval/benchmark.py  --model vit_base_patch16_siglip_224 --preprocess gpu --max 3 --trt_instance_num 2 --timeout 5 --preprocess-instances 6 --total_number 1000000 --client 10 --save stability.log
+
+
+# resnet
+
+python decouple_eval/benchmark.py  --model resnet101 --preprocess cpu --max 5 --trt_instance_num 2 --timeout 5 --preprocess-instances 6 --total_number 1000000 --client 14 --save stability.log
+
+
+python decouple_eval/benchmark.py  --model resnet101 --preprocess gpu --max 5 --trt_instance_num 2 --timeout 5 --preprocess-instances 6 --total_number 1000000 --client 14 --save stability.log
+
 
 # result
 cat stability.log
