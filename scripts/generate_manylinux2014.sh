a4bdd2ad7dea5b76cd1f2e6f07d75198f7024ea7
tpnan <hydzhang@hotmail.com>
2025-11-06 17:46:37 +0000

    git

--- scripts/generate_manylinux2014.sh
+++ scripts/generate_manylinux2014.sh
@@ -5,6 +5,7 @@ rm -rf wheelhouse/*manylinux2014_x86_64.whl
 rm -rf hami*.egg-info/*
 
 git config --global --add safe.directory .
+git fetch --unshallow --tags 2>/dev/null || true
 
 export PYVER=313
 
@@ -18,7 +19,7 @@ for PYVER in 38 39 310 311 312 313; do
     pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 
     /opt/python/cp${PYVER}-cp${PYVER}/bin/python3  -m pip install build pybind11 auditwheel-symbols setuptools setuptools_scm
-    USE_CXX11_ABI=0 /opt/python/cp${PYVER}-cp${PYVER}/bin/python3 setup.py -q bdist_wheel 
+    USE_CXX11_ABI=0 /opt/python/cp${PYVER}-cp${PYVER}/bin/python3 setup.py bdist_wheel 
 
     auditwheel repair --plat manylinux2014_x86_64 dist/hami*-cp$PYVER-cp$PYVER-linux_x86_64.whl 
 
