064dbc9b9cbf374bdde01326c3b669306cc028c9
tp-nan <hydzhang@hotmail.com>
2026-02-02 01:23:06 +0800

    [JIT]

--- plugins/torchpipe/torchpipe/utils/_build_trt.py
+++ plugins/torchpipe/torchpipe/utils/_build_trt.py
@@ -99,7 +99,7 @@ def get_trt_url():
         cuda_urls = ["https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.5.0/tars/TensorRT-10.5.0.18.Linux.x86_64-gnu.cuda-12.6.tar.gz",
                      "https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/9.3.0/tensorrt-9.3.0.1.linux.x86_64-gnu.cuda-12.2.tar.gz",
                      "https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.14.1/tars/TensorRT-10.14.1.48.Linux.x86_64-gnu.cuda-12.9.tar.gz"]
-        return cuda_urls[1]
+        return cuda_urls[0]
     elif cuda_version == 13:
         cuda_urls = [
             "https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.14.1/tars/TensorRT-10.14.1.48.Linux.x86_64-gnu.cuda-13.0.tar.gz"]
